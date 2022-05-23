b775da397edd369ea79c9f5f7569979feac7bbc9
Talex210 <tal.89@mail.ru>
2022-05-23 12:56:28 +0300

    Сверстал макет

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            Хостим React приложение
+        <div className='app-wrapper'>
+            <div className='list-elements'>
+                <p>label</p>
+                <p>id</p>
+                <p>more</p>
+            </div>
+            <div className='additional-information'>Дополнительная информация об элементе</div>
+            <div className='buttons'>
+                <button style={{marginRight: '10px'}}>Refresh</button>
+                <button>Remove</button>
+            </div>
         </div>
     );
 }
